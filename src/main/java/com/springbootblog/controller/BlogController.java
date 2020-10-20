f2e5bbfa9ceebfcf07f6e44ee1bc1b51349b63a5
Alexander Gerber <alexander.gerber@check24.de>
2020-10-20 20:23:52 +0200

    add database#

--- src/main/java/com/springbootblog/controller/BlogController.java
+++ src/main/java/com/springbootblog/controller/BlogController.java
@@ -4,6 +4,7 @@ import com.springbootblog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -14,6 +15,7 @@ public class BlogController {
     private BlogService blogService;
 
     @GetMapping(value = "/")
+    @ResponseBody
     public String index() {
         return "Welcome to RestTemplate Example.";
     }
